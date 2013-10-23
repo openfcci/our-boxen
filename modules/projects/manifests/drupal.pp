437c4e3f9da694ae8ffcb509283602988f48c8b4
Brendan Tobolaski <brendan@tobolaski.com>
2013-10-23 11:15:00 -0500

    Added packer for drupal.

--- modules/projects/manifests/drupal.pp
+++ modules/projects/manifests/drupal.pp
@@ -1,11 +1,19 @@
 class projects::drupal {
     include chrome
     include firefox
+    include homebrew
     include sequel_pro
     include virtualbox
     include vagrant
 
     vagrant::plugin { 'vagrant-mountcommand':
+    }
+
+    homebrew::tap { 'homebrew/binary': }
+
+    package {
+      'homebrew/binary/packer':
+        ensure => '0.3.7';
     }
 
     boxen::project { 'fcc-drupal-cms':
