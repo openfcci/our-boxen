da8773005a2e2823a1610ffb8f0a17bb4a912577
Brendan Tobolaski <brendan@tobolaski.com>
2013-10-31 13:30:31 -0500

    Upgrade the required packer for drupal.

--- modules/projects/manifests/drupal.pp
+++ modules/projects/manifests/drupal.pp
@@ -13,7 +13,7 @@ class projects::drupal {
 
     package {
       'homebrew/binary/packer':
-        ensure => '0.3.9';
+        ensure => '0.3.10';
       'siege':
         ensure => '3.0.5';
       'phantomjs':
